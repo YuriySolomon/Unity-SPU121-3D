24200c79c0838e7935e23aefc27b649422791734
Юрій Соломон <tsar.povelevaet@gmail.com>
2023-12-26 01:05:57 +0200

    3D-Finish

---
+++ Assets/Scripts/ClockScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockScript : MonoBehaviour
+{
+    [SerializeField]
+    TMPro.TextMeshProUGUI clock;
+
+    private float time;
+
+    void Start()
+    {
+        time = 0f;    
+    }
+        
+    void Update()
+    {
+        time += Time.deltaTime;
+    }
+	private void LateUpdate()
+	{
+		//clock.text = time.ToString();
+		clock.text = FormatTime(time);
+	}
+	private string FormatTime(float totalSeconds)
+	{
+		TimeSpan timeSpan = TimeSpan.FromSeconds(totalSeconds);
+		return $"{timeSpan.Hours:D2}:{timeSpan.Minutes:D2}:{timeSpan.Seconds:D2}.{timeSpan.Milliseconds / 100}";
+	}
+}
+/* Д.З. Завершити роботу над проєктом 3D
+ */
---
+++ Assets/Scripts/FinishScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishScript : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }    
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{		
+		bool result = EditorUtility.DisplayDialog("Повыдомлення", "Ви пройшли рівень. Перейти на наступний?", "OK", "Cancel");
+
+		// Реагуємо на вибір у діалоговому вікні
+		if (result)
+		{
+			SceneManager.LoadScene("Level-2");
+		}
+		else
+		{			
+			EditorApplication.isPlaying = false;
+		}
+	}
+}
